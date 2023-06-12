00b8806de9f58d043597276ef88625b305dbe1a6
Nuria Díaz Sánchez <nuriadiazsanchez91@gmail.com>
2023-06-12 17:08:33 +0200

    edit ficha mascota

--- app/content/mascotas/ficha_mascota.php
+++ app/content/mascotas/ficha_mascota.php
@@ -38,10 +38,11 @@ $dia = substr($mascota->fecha_nac, 8, 2);
     <div class="row">
         <div class="col-md-6 offset-md-3">
             <div class="card">
-                <div class="card-header">
-                    <h2 style="float:left"><?= $mascota->nombre ?></h2>
-                    <div class="diviconorrss">
-                        Comparte:&nbsp;&nbsp;
+                <div style="padding-bottom: 0px;" class="card-header">
+
+                    <h2 class="display-6" style="float:left "><?= $mascota->nombre ?></h2>
+                   
+                    <div style="margin-top: 1px;" class="diviconorrss">
                         <a style="text-decoration:none" href="https://twitter.com/intent/tweet?text=¡Ayuda a cambiar la vida de un animal necesitado! <?=$mascota->nombre?> está en busca de un hogar, visita nuestra página y conócela&url=http%3A%2F%2Flocalhost%2FAdopcionesApp_3.0%2Fapp%2Fcontent%2Fmascotas%2Fficha_mascota.php&via=adopcionesApp&hashtags=Adopción,Mascotas" target="_blank">
                             <img class="iconorrss" src="../../../assets/images/logos/rrss/twitter.png" alt="">
                             <a style="text-decoration:none" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%2FAdopcionesApp_3.0%2Fapp%2Fcontent%2Fmascotas%2Fficha_mascota.php&display=popup" target="_blank">
@@ -50,10 +51,14 @@ $dia = substr($mascota->fecha_nac, 8, 2);
 <a href="whatsapp://send?text=¡Ayuda a cambiar la vida de un animal necesitado! <?=$mascota->nombre?> está en busca de un hogar, visita nuestra página y conócela%0A%0Ahttp%3A%2F%2Flocalhost%2FAdopcionesApp_3.0%2Fapp%2Fcontent%2Fmascotas%2Fficha_mascota.php" target="_blank">
 
 <img style="text-decoration:none;  width: 27px; height: 27px;" class="iconorrss" src="../../../assets/images/logos/rrss/whatsapp.png" alt="">
-</a>
+</a><div style="margin-bottom: 1px;" class=""> <p style="margin-bottom: 4px;font-size:12.5px;color:#00afa1" class="">¡Comparte y ayuda a encontrar <br>un hogar a <?= $mascota->nombre ?>!</p></div>
 
                     </div>
+                    <br>
+                    
+                    
                 </div>
+                
                 <div class="card-body">
                     <div class="animal-image">
                         <img style="height:500px;" src="<?php echo "../../../assets/images/mascotas/" . $mascota->especie . "/" . $mascota->microchip . "." . $mascota->extension_imagen ?>" class="card-img" alt="Imagen del Animal">
