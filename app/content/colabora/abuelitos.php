7f881514a60fc79af7ee3b7d0a15a1c3f0c359df
SUAREZ ARQUERO Esla <esla.suarez2@soprasteria.com>
2023-06-20 14:55:48 +0200

    abuelitos front

--- app/content/colabora/abuelitos.php
+++ app/content/colabora/abuelitos.php
@@ -2,84 +2,45 @@
 
 <div id="contenido">
   <div class="voluntariado">
-    <div class="row">
-      <div class="col-md-6">
-        <h3>Adopta un abuelito</h3>
-        <ul class="list-unstyled">
-          <p>Quizá la situación más triste que se nos presenta en AdoptApp es rescatar un animal de la calle y de una vida llena de desgracias y sinsabores y que acabe sus días en el Centro de Adopción sin haber podido conocer el calor, los mimos, el cariño y las alegrías de un hogar.</p>
-          <p>Encontrar adoptantes para los animales viejitos es complicado, las familias suelen preferir cachorros o animales jóvenes para compartir más tiempo con ellos. Sin embargo, olvidamos que el tiempo que compartamos con un viejito será mucho más intenso, su capacidad de agradecimiento es infinita y sus miradas muy dulces porque ya no tienen que demostrar nada, sólo querer y dejarse querer. Son animales que en su mayoría ya están educados lo que facilita la etapa de adaptación, son tranquilos y exigen algo menos de actividad. Lamentablemente cada día que pasan en el Centro se esfuma un porcentaje muy alto de su tiempo para disfrutar.</p>
-          <p>Te proponemos que seas su gran oportunidad. Acoge a uno de nuestros viejitos bien por un tiempo indefinido, bien hasta su fallecimiento. En el caso de la primera opción, nosotros seguiremos intentando encontrarle un hogar definitivo pero dale, desde ya, la suerte de cariños cercanos, cama blandita, paseos, amigos y mimos que tanto necesitan.</p>
-          <p>Desde AdoptApp cubriremos todas sus necesidades de alimentación, atención veterinaria, medicación caso de necesitarlo y residencia u otra casa, durante los días que tengas que ausentarte.</p>
-        </ul>
-        <a href="#" class="boton-animales">Conoce a los abuelitos</a>
-      </div>
-      <div class="col-md-6">
-        <div class="formulario_abuelitos">
-          <h3>¿Cómo puedo ayudar a un abuelito?</h3>
-          <form>
-      <div class="mb-3">
-        <label for="nombre" class="form-label">Nombre</label>
-        <input type="text" class="form-control" id="nombre" name="nombre" required>
-      </div>
-      <div class="mb-3">
-        <label for="apellidos" class="form-label">Apellidos</label>
-        <input type="text" class="form-control" id="apellidos" name="apellidos" required>
-      </div>
-      <div class="mb-3">
-        <label for="telefono-movil" class="form-label">Teléfono móvil</label>
-        <input type="tel" class="form-control" id="telefono-movil" name="telefono-movil" required>
-      </div>
-      <div class="mb-3">
-        <label for="telefono-fijo" class="form-label">Teléfono fijo</label>
-        <input type="tel" class="form-control" id="telefono-fijo" name="telefono-fijo" required>
-      </div>
-      <div class="mb-3">
-        <label for="direccion" class="form-label">Dirección</label>
-        <input type="text" class="form-control" id="direccion" name="direccion" required>
-      </div>
-      <div class="mb-3">
-        <label for="codigo-postal" class="form-label">C. P.</label>
-        <input type="text" class="form-control" id="codigo-postal" name="codigo-postal" required>
-      </div>
-      <div class="mb-3">
-        <label for="poblacion" class="form-label">Población</label>
-        <input type="text" class="form-control" id="poblacion" name="poblacion" required>
-      </div>
-      <div class="mb-3">
-        <label for="provincia" class="form-label">Provincia</label>
-        <input type="text" class="form-control" id="provincia" name="provincia" required>
-      </div>
-      <div class="mb-3">
-        <label for="email" class="form-label">Tu correo electrónico</label>
-        <input type="email" class="form-control" id="email" name="email" required>
-      </div>
-      <div class="mb-3">
-        <label for="horario-trabajo" class="form-label">Horario de trabajo</label>
-        <input type="text" class="form-control" id="horario-trabajo" name="horario-trabajo" required>
-      </div>
-      <div class="mb-3">
-        <label for="otros-animales" class="form-label">Hay otros animales en casa</label>
-        <select class="form-control" id="otros-animales" name="otros-animales" required>
-          <option value="si">Sí</option>
-          <option value="no">No</option>
-          <option value="dispuesto">Dispuesto a tener</option>
-        </select>
-      </div>
-      <div class="mb-3">
-        <label for="comentarios" class="form-label">Comentarios</label>
-        <textarea class="form-control" id="comentarios" name="comentarios" rows="3"></textarea>
-      </div>
-      <div class="form-check mb-3">
-        <input class="form-check-input" type="checkbox" id="terminos-condiciones" required>
-        <label class="form-check-label" for="terminos-condiciones">
-          He leído y acepto los términos y condiciones de uso.
-        </label>
-      </div>
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+        <div class="container">
+
+        <div class="row">
+          <div class="column-header col">
+            Adopta un abuelito
+          </div>
+          <div class="column-header col">
+            ¿Quieres saber cómo ayudar a los animales más mayores?
+          </div>
         </div>
+
+        <div class="row">
+          <div class="column-body body-1 col">
+            Quizá la situación más triste que se nos presenta en AdoptApp es rescatar un animal de la calle y de una vida llena de desgracias y sinsabores y que acabe sus días en el Centro de Adopción sin haber podido conocer el calor, los mimos, el cariño y las alegrías de un hogar
+          </div>
+          <div class="column-body body-2 col">
+            Encontrar adoptantes para los animales viejitos es complicado, las familias suelen preferir cachorros o animales jóvenes para compartir más tiempo con ellos. Sin embargo, olvidamos que el tiempo que compartamos con un viejito será mucho más intenso, su capacidad de agradecimiento es infinita y sus miradas muy dulces porque ya no tienen que demostrar nada, sólo querer y dejarse querer. Son animales que en su mayoría ya están educados lo que facilita la etapa de adaptación, son tranquilos y exigen algo menos de actividad. Lamentablemente cada día que pasan en el Centro se esfuma un porcentaje muy alto de su tiempo para disfrutar.
+          </div>
+          <div class="col">
+                <img src="https://images.unsplash.com/photo-1603459806507-b6c21fab1d1c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=825&q=80" alt="elderly-dog" style="width:350px;height:350px;">
+            </div>
+          <div class="row">
+            
+          </div>
+        </div>
+
+        <div class="row">
+            <div class="column-body body-3 col">
+                Te proponemos que seas su gran oportunidad. Acoge a uno de nuestros viejitos bien por un tiempo indefinido, bien hasta su fallecimiento. En el caso de la primera opción, nosotros seguiremos intentando encontrarle un hogar definitivo pero dale, desde ya, la suerte de cariños cercanos, cama blandita, paseos, amigos y mimos que tanto necesitan.
+            </div>
+            <div class="column-body body-4 col">
+                Desde AdoptApp cubriremos todas sus necesidades de alimentación, atención veterinaria, medicación caso de necesitarlo y residencia u otra casa, durante los días que tengas que ausentarte. Haz una solicitud a través del formulario de adopción, indicando que estás interesado/a en adoptar un abuelito. 
+                <a href="adopta.php" class="boton-animales">Recibir más información</a>
+            </div>
+          </div>
       </div>
-    </div>
+
+
+      </style>
   </div>
 </div>
 
@@ -91,3 +52,77 @@
 
 	?>
 	</div>
+      <style>
+
+        .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.column-header,
+.column-body {
+  margin: 3px;
+}
+
+.column-header {
+  padding: 35px;
+  /* background-color: #fb9ee7; */
+  background-color: #fff;
+  color: #000;
+  font-size: 25px;
+  text-align: center;
+  font-weight: bold;
+  border: solid 3px #d97bb2;
+}
+
+.body-1 {
+  background-color: #CCFBFE;
+  color: #333;
+  padding: 20px;
+}
+
+.body-2 {
+  background-color: #CDACA1;
+  color: #333;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.body-3 {
+  background-color: #ffdab9;
+  color: #333;
+  padding: 20px;
+  border-radius: 10px;
+}
+.body-4 {
+  background-color: #ffdab9;
+  color: #333;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #FCF6BD;
+}
+
+
+
+.row .col {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  
+}
+
+.row img {
+  width: 350px;
+  height: 350px;
+  object-fit: cover;
+  border-radius: 10px;
+}
+
